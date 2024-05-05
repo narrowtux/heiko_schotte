a6e2d9d921d5e36903dc263af16bc1b3d5ffa472
Moritz Schmale <narrow.m@gmail.com>
2024-05-05 13:23:54 +0200

    add docker compose up -d

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 
 docker build -t hh_discord_app .
 docker save hh_discord_app:latest | ssh -C root@37.221.193.242 "docker load"
+ssh root@37.221.193.242 "cd /opt/hh_discord_app; docker compose up -d"
